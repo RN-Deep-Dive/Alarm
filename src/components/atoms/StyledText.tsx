605438a6594b0b3fb156a4bbf92b469bf059fb12
ywonchae1 <ywonchae62@gmail.com>
2023-12-23 11:29:35 +0900

    #16 feat: problemSet 기본 형태

--- src/components/atoms/StyledText.tsx
+++ src/components/atoms/StyledText.tsx
@@ -13,7 +13,9 @@ import styled from "styled-components/native";
 
 const SText = styled.Text`
   font-size: ${(props) => {
-    if (props.fontSize === "md") {
+    if (props.fontSize === "xs") {
+      return `${spacing.xs}px`;
+    } else if (props.fontSize === "md") {
       return `${spacing.md}px`;
     } else if (props.fontSize === "lg") {
       return `${spacing.lg}px`;
@@ -25,7 +27,7 @@ const SText = styled.Text`
   font-weight: ${(props) => (props.isBold ? "bold" : "normal")};
 `;
 
-type FontSize = "md" | "lg";
+type FontSize = "xs" | "md" | "lg";
 
 interface StyledTextProps {
   text?: string;
@@ -34,6 +36,14 @@ interface StyledTextProps {
   isBold?: boolean;
   onPress?: () => void;
 }
+
+/**
+ * @param {string} text - 표시할 텍스트.
+ * @param {FontSize} fontSize - 텍스트의 폰트 크기 ('md', 'lg' 중 하나).
+ * @param {string} color - 텍스트의 색상.
+ * @param {boolean} isBold - 텍스트를 굵게 할지 여부.
+ * @param {Function} onPress - 텍스트를 눌렀을 때 실행할 함수.
+ */
 
 const StyledText: React.FC<StyledTextProps> = ({
   text,
--- src/screens/ProblemScreen.tsx
+++ src/screens/ProblemScreen.tsx
@@ -5,29 +5,18 @@ import MainBtn from "../components/atoms/MainBtn";
 import { colors } from "../constants/colors";
 import StyledText from "../components/atoms/StyledText";
 import { FloatingBtn } from "../components/atoms/FloatingBtn";
+import ProblemSet from "../components/molecules/ProblemSet";
 
 const ProblemScreen: React.FC<
   NativeStackScreenProps<any, "ProblemScreen">
 > = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center", position: "relative" }}>
-      <MainBtn
-        text={"비상 탈출 버튼"}
-        onPress={() => { }}
-        subText={"1/299회 누름"}
-        color="red"
+      <ProblemSet
+        title='hello'
+        desc='hahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahaha'
+        onPress={() => console.log('hello')}
       />
-
-      <StyledText
-        text={"hello"}
-        fontSize="md"
-        color={colors.textDim}
-        onPress={() => {}}
-      />
-
-      <Text>ProblemScreen</Text>
-      <FloatingBtn onPress={() => console.log('hello')} iconName="plus" iconSize={15} />
-
     </View>
   );
 };
